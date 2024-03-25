e5604222725e4719d52e59c8eec0111b8512855f
Elvis <elvisngerem@gmail.com>
2024-03-25 21:55:13 +0100

    My Portfolio

---
+++ index.js
@@ -0,0 +1,26 @@
+// string 
+// numbers 
+// int 
+// boolean 
+// object 
+// array
+
+let greetings= "How are you this morning?"
+const hello = "Hi, Elvis"
+
+console.log(hello)
+
+let x = 500
+let y = 900
+const total = x+y
+
+console.log("The total is "+ total)
+
+let class1 = true
+let class2 = false
+
+if (class1 > class2){
+    console.log('class 1 is false')
+}else{
+    console.log("class 1 is not false")
+}
